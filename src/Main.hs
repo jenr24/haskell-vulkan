4292ceb2b95a19315945d837c3e8bc529508c61c
Jen Reiss <jenc.reiss24@gmail.com>
2022-03-16 11:07:17 -0500

    more

---
+++ src/Main.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeApplications #-}
+module Main where
+    
+import Data.Kind (Type)
+import Control.Algebra 
+import Control.Carrier.State.Strict (State, get)
+import Control.Carrier.Fail.Either
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
+
+data LogTarget a where
+    NOP 
+        :: LogTarget a
+    VAR 
+        :: Monoid a 
+        => a 
+        -> LogTarget a
+
+data Logger a (m :: Type -> Type) k where
+    Warning :: a -> Logger a m ()
+    Error :: a -> Logger a m ()
+    Info :: a -> Logger a m ()
+
+
+warning 
+    :: (Has (Logger a) sig m)
+    => a -> m ()
+warning = send . Warning
+
+error
+    :: (Has (Logger a) sig m)
+    => a -> m ()
+error = send . Error
+
+info
+    :: (Has (Logger a) sig m)
+    => a -> m ()
+info = send . Info
+
+newtype LoggerC a m b = LoggerC { runLogger :: m b }
+    deriving (Functor, Applicative, Monad)
+
+instance (
+    Has (State (LogTarget a)) sig m,
+    Algebra sig m ) => Algebra (Logger a :+: sig) (LoggerC a m) where
+        alg hdl sig ctx = case sig of 
+            L action -> do
+                target <- get
+                case target of
+                    NOP -> return ()
+                    VAR x -> return ()
+            R other -> LoggerC (alg (runLogger . hdl) other ctx)
